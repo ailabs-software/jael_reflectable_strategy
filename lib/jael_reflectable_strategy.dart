8246254afbd98bf4cca1f8534d69b947bf40b512
Sheyenne York <support@bpilotglobal.com>
2020-01-09 00:21:36 -0500

    Implement

--- lib/jael_reflectable_strategy.dart
+++ lib/jael_reflectable_strategy.dart
@@ -1,8 +1,18 @@
 import "package:jael/src/member_resolver.dart";
+import "package:reflectable/reflectable.dart";
 
 /** @fileoverview Implements member resolution strategy using package:reflectable */
 
-class JaelReflectable
+class ReflectableMemberResolver implements IMemberResolver
 {
+  final Reflectable _reflectable;
 
+  ReflectableMemberResolver(Reflectable this._reflectable);
+
+  @override
+  Object getMember(Object target, String name)
+  {
+    InstanceMirror instanceMirror = _reflectable.reflect(target);
+    return instanceMirror.invokeGetter(name);
+  }
 }
